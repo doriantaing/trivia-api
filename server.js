9a3f19e8c15c1c4ce66b8c25b7238f8bc6dfa8ba
Dorian Taing <doriantaing77@hotmail.fr>
2019-01-12 17:40:36 +0100

    add allow access header

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const port = process.env.PORT || 5000;
 const categoryJson = require('./category.json');
 const categoriesJson = require('./categories.json');
 
-
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "https://sushi-gemu.doriantaing.fr/");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+ });
 
 app.route('/api/category')
    .get( (req, res) => {
