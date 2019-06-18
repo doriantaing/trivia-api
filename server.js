175526db425535919b29c62f04f20669650a0108
Dorian Taing <doriantaing77@hotmail.fr>
2019-06-18 23:41:06 +0200

    connection sql && route categories

--- server.js
+++ server.js
@@ -3,12 +3,46 @@ const app = express();
 const port = process.env.PORT || 5000;
 const categoryJson = require('./category.json');
 const categoriesJson = require('./categories.json');
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hetic',
+    database: 'sushi_gemu'
+});
+
+conn.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('Connected to database');
+});
 
 app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "https://sushi-gemu.doriantaing.fr");
+//    res.header("Access-Control-Allow-Origin", "https://sushi-gemu.doriantaing.fr");
+   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
  });
+
+app.route('/api/categories').get((req , res) => {
+    const data = [];
+    const sqlQuery = `
+        SELECT category
+        FROM categories
+    `;
+
+    conn.query(sqlQuery, (err, rows, fields) => {
+        if (err) throw err;
+        rows.map(el => {
+           data.push(el)
+        });    
+        
+        res.status(200).json({data})
+    });
+    
+    // conn.end();
+});
 
 app.route('/api/category')
    .get( (req, res) => {
